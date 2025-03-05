61ef6a9b16eafbe75394e3c8921cd8c9c3a1a843
Christabel Osei <armahsarpomaa16@gmail.com>
2025-03-05 16:15:23 -0600

    Defined the Email Department

--- Email_Application/Email.java
+++ Email_Application/Email.java
@@ -1,5 +1,7 @@
 package Email_Application;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstname;
     private String lastname;
@@ -13,9 +15,27 @@ public class Email {
         this.firstname = firstname;
         this.lastname = lastname;
         System.out.println("EMAIL CREATED: "+ this.firstname + " "+ this.lastname);
+
+        //Call a methond to ask for the department: return the department
+        this.department = setDepartment();
+        System.out.println("Department: "+ this.department);
     }
 
     // Ask for the department 
+    private String setDepartment(){
+        System.out.print("DEPARTMENT CODES\n1 for Sale\n2 for Developement\n3 for Accounting\n0 for none\nEnter departement code:");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1){
+            return "sales";
+        }else if (depChoice == 2) {
+            return "Dev";
+        }else if(depChoice == 3){
+            return "Account";
+        }else{
+            return "";
+        }
+    }
 
     //Generate random password 
 
