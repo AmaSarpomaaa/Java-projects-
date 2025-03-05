fcfcb3a0cf146f319fba2de8fbd28f0474cb3bec
Christabel Osei <armahsarpomaa16@gmail.com>
2025-03-05 15:47:36 -0600

    Created the outline for the Email Admin Class

---
+++ Email Application/Email.java
@@ -0,0 +1,21 @@
+public class Email {
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+    //Constructor to recive the first and last name 
+
+    // Ask for the department 
+
+    //Generate random password 
+
+    //Set the mailbox capacity 
+
+    //Set the alternate email
+
+    //Change the password
+
+}
